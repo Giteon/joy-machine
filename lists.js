586c660a9276ac292b572be170a9e793e4c977e3
Giteon <fox.gideon@gmail.com>
2025-09-13 16:55:14 -0400

    init

---
+++ lists.js
@@ -0,0 +1,386 @@
+// Word lists for the Pure and Infinite Joy Machine
+const listsText = `INTRO (I)
+fucking ultimate chicken
+fucking chicken horse
+ultimate fucking
+fucking horse chicken
+ultimate chicken time
+ultamate
+fucking
+ultimate chicken
+horse ultimate
+ultimate
+ultimate chicken
+ultimate
+
+BEAST (B)
+[secret, *B*] aardvark
+aligator [lounge, *B*]
+bear
+bumblebee
+butterfly
+capybara
+cockroach
+[cocaine, *B*] dog
+crab
+crocodile [dundee, *B*]
+currasow
+cow
+[disco, *B*] snail
+deer [in headlights, *B*]
+dodo
+dog
+dolphin
+donkey [kong [jerking off face down, *F*]]
+duck
+elephant
+ferret
+fish [neck, *B*]
+fox
+frog
+gerbil
+giraffe
+giraffe
+goat
+godzilla
+goldfish
+goose
+gorilla
+hamster
+hedgehog
+hippo
+horse
+jellyfish
+kangaroo
+koala
+ladybug
+lemur
+llama
+lobster [joaquin phoenix, .2, *B*]
+meerkat
+monkey [pox, *B*]
+moth
+nintendog
+octopus
+pigeon
+parrot
+penguin
+platypus
+pterodactl
+python
+pig
+rabbit
+raccoon
+rat [bones, *B*]
+rooster [head in dirt, *B*]
+sasquatch
+scorpion
+sheep
+skunk
+snake
+spider
+sphinx
+squirrel
+tiger
+turtle
+turkey
+walrus
+whale
+wolf
+zebra
+
+
+NOUN (N)
+needle beach
+bound to wheelchair
+full-body (paralysis/orgasm)
+imitation holocaust crab
+sydney sweeney [bone orchid [immune from all disease, *F*]]
+mozzarella stick
+hinge premium [diahhrea from tube]
+[45°, .2] cock shame
+GROK
+propranolol [8000mg]
+coke zero [friends or family, *F*]
+nutritional yeast [infection, .3]
+gents blend
+syndicated [television]
+shiva baby
+selena gomez [oreo mmmm tastes like fucking Mexico everything is in sepia now, *F*]
+dorito pilgrim [thanksgiving, .25]
+black mirror
+9/11 (simulator/livestream)
+sophie's choice
+spotify premium
+popcorn lung
+barmitzvah
+ted cruz [donde esta la biblioteca, *F*]
+lobotomy
+slutty vegan
+above-ground (burial/grave)
+pussy taste test [jump up and down, .3, *F*]
+Rodolfo [trapped in floorboards, *F*]
+primordial soup
+laura palmer [body bag, .25 [dream sequence, .25]]
+war movie monday
+u sure? 😏😏😏
+saggitarius [rising]
+citronela ring pop
+kim petras [coconut [perfect pitch, .2]]
+kurt vonnegut
+goblin mode
+breastmilk [on the cheap, .25]
+hungry tax [press button to feed nutrition cube, *F*]
+schoolbus explosion [zero surviviors [4k, *F*]]
+QS watermelon
+wet leg
+thumbalina
+wartime economy
+head exploded [no pulse]
+with 9 billion child IQ
+intuit turbotax [hungry tax]
+monster porn
+french red [frozen solid, .25]
+precum [premarket [postmarket]]
+spike the punch
+instant death
+iranian steakhouse
+cardio mezzanine [death by hypoxia, .3 [no response to thoughtful email, *F*]]
+thimble of milk
+syndicated television
+bisexual boyfriend [(trough/simulator)]
+orgasmatron 3000
+topamax sprinkle [300,000mg [lethal dose, *F*]]
+ratatouille [magic cheese]
+[cleopatra] past life regression [ran over by car [500 times] *F*]
+boing boing yoga ball [blacked out [no memories], *F*]
+ikea chair [sabrina carpenter creaming jeans, *F*]
+walter goggins [aimee lou wood [nipple scene uncensored, *F*]]
+show of force
+
+CANDY(C)
+scrub a dub dub
+bombaclat [ultimate selector [WITH THE CHAMPION SOUND, *F*]]
+pink (ponk/pony club)
+perfect politics
+jamba juice [ecoli]
+buccal fat [tummy tuck]
+freaky friday
+tip-to-taint
+laffy taffy
+she-shed [wasp infestation]
+bubble butt [bazonga]
+[fucking] candy flip
+ding dong [ditch]
+54321 [54321]
+gamer gunk
+gutterball
+wet nurse [japanese sponge bath, *F*]
+ozempic (summer, .8/winter, .2)
+yankee doodle [dandy, .8, *F*]
+[pussy popping, *F*] polly pocket
+bunson burner [bitch]
+hellen keller [jerking off all over town, *F*]
+magic mike [wazowski, .2]
+scooby doo [velma 4k stuck in washing machine, .2, *F*]
+tiny desk
+chester cheeta
+dunkin donut[s]
+zoboomafoo
+beach blanket babylon
+baby bonnet
+labubu [lafufu [lagoogoo]]
+bing bong
+cottage cheese
+pussy slay mama
+daily dingle
+party popper
+willy wonka
+squink squonk
+yuppy bitch
+baba yaga
+wawa
+slop job
+jungle juice
+bazonga
+
+ADJECTIVE (A)
+blood (drenched/soaked)
+poisonous [riddled with (disease/needles), *F*]
+fuck-ass
+le epic [pwn, .2]
+full-pen [summer[time sadness, *F*]]
+gone sexual [gone missing, .2]
+boston dynamics
+low libido (magic the gathering/penis boy)
+[sort of an, .2, *F*] ethnic twist
+chained to radiator
+authentic home-cooked
+animal style
+type-3
+I-Spy
+180p
+clown
+quirked up [white boy with a little bit of swagger busts it down sexual style, *F*]
+benegesserit
+bitch[-ass, .2]
+funtivity [by hermis, *F*]
+egg-laden [females]
+clown [party, .3]
+knee-high [swampwater]
+for her pleasure
+lifetime subscription
+sucked dry
+bottled from the source
+farm to table
+victory royale
+cum-colored
+zero swag [detected]
+hath risen [on the third day, .2, *F*]
+family plan
+kill on site
+hong-kong style
+9 trillion dead
+[fucking] color blind
+super-auto [fucking]
+touch-based non-sexual [non-(poly/monog) partnership, *F*]
+ontologically (evil/sexy/bad person)
+submissive [breedable, .2]
+command position
+michael buble [compilation [no clothes, *F*]]
+open-concept [torture dungeon]
+slut [whore [bitch [cunt]]]
+paradigm shift
+green bubble 
+boy phone girl phone
+ahegao [mommy milker [sudoku eyes, 0.6, *F*]]
+
+FINALE (F)
+time
+YOUNG [(Skylar Gisondo/Jenna Orteg/Coleman Domingo) YOUNG, .2]
+cat kitty cat cat kitty cat cat
+dot com [dot jizz [dot edu]]
+total body enhancement [chamber [of commerce]]
+(reliable, expensive, dangerous)
+with the (fat naturals, .7/fatties, .3)
+3-trillion thread count
+shit cup runneth over [my balls, .3]
+SEND [UNSEND SEND [FUCK]]
+THAT COULD BE A SHOW
+NUKE IN DA CLUB [HA HA, .3]
+switch it up like nintendo [I MISS US]
+face down stroking my shit [wrist fractured [8 month recovery]]
+ten times tintinuous
+handed 15 cellphones pushed into pool when least expected
+SPRING BREAK VACATION IN DARIAN GAP
+fuckkkk id do anything for it
+currasow waiting outside room [death immenent [stuck in room 16 hours]]
+where my hug at?
+fucking turn around bend over [lemme see the nutrition facts]
+trail gone cold [dog dead [no GF]]
+MAYO MOMENT
+how many calories in 30 advil
+LOL (LOTS OF LOVE)
+infertile if dog
+etc etc and so on in such a way
+and subway take away everything you love
+what is bro doing
+mmmm ice cold water mmmmm[mmmmmmm]
+mmmmmmm mmmmm let me imagine it
+where did these bullet holes come from
+and let the [fucking] bunny eat the carrot
+RICH MILLIONAIRE
+at least that's what they're saying [i dont fucking know]
+THE CITY IS MINE
+your skin oh yeah your skin and bones, turn in to something beautiful
+in good christian fun
+emits poweful pheromone brain scrambled
+within 1000 feet of a school
+take off glasses it is revealed she is actually beautiful
+DANGER WILL ROBINSON [DANGER [DANGER]]
+100 concubines (NOT virgins, robust sexual references)
+🥺🥺🥺
+leave no trace search history wiped
+why buy the cow when you can have the milk for free
+in 5-7 business days
+using a complex system of pulleys and levers
+10 lashings goddess
+oh nooo oh fuckkk sorry kareem i ate all the drywall I thought it was cottage cheeseeee
+[ok sorry goddess, .2] yes chef sorry chef
+we'll take the check
+translated to (chinese/german/icelandic/hungarian/japanese/korean/portuguese/russian/spanish/swedish/turkish/vietnamese) 30 times
+for up to one year [in a shipping container, .8]
+weeeeeeeeeeeeee
+dave & busters [dave & busters dave & busters, .15]
+and SCENE
+walk the plank [bitch, .4]
+RELEASE THE SNAKES
+but thats up to you
+por favor
+no refunds
+Siri when is July [oops unsend [haha. siri hold my calls]]
+NIGHTMARE NIGHTMARE
+fuck my fucking ass
+shirley temple microwave 45 minutes HOT
+im going to take this back to my home lab
+I guess
+anti-(hoes/happiness) technology
+girlfriend (who loves me [so much], .7/dating sim, .3)
+IN ENGLISH PLEASE
+ChatGPT 1 million almonds destroyed
+SCRONGIEEEEE
+plus tax and tip
+how can i be homophobic my bitch is gay
+round up to nearest billion yuan
+slumber party hang upsidedown and cut major arteries to drain blood as fast as possible
+xiaohongshu
+on my desk YESTERDAY
+green moose, guava juice large fries, chocolate shake
+300hz binaural beats [mozart gray noise flow state] for study work migraine focus [cure vision]
+japanese water torture [with spritz element]
+jarvis jerk it a little 😏
+MAN I GOT IT MADE
+guantanamo bay forced to watch pole dancing video on work computer
+retweet 1000
+and buy yourself something nice kid
+10lbs soaking wet [no reciept]
+by way of demonstration to find most trusted advisor
+[and consider organizing a regular] ICE CHIPPING CHALLENGE
+put the fries in the bag bro
+SLIME MAN`;
+
+
+// Parse the text into arrays
+const parseLists = (text) => {
+  const sections = text.split('\n\n');
+  const lists = [];
+  
+  sections.forEach(section => {
+    const lines = section.trim().split('\n');
+    const items = [];
+    
+    lines.forEach(line => {
+      // Skip section headers (lines in CAPS)
+      if (line.match(/^[A-Z\s\(\)]+$/)) {
+        return;
+      }
+      
+      // Skip empty lines
+      if (!line.trim()) {
+        return;
+      }
+      
+      // Add the line as an item (no numbers to remove)
+      items.push(line.trim());
+    });
+    
+    if (items.length > 0) {
+      lists.push(items);
+    }
+  });
+  
+  return lists;
+};
+
+// Create the word lists
+window.wordLists = parseLists(listsText);
